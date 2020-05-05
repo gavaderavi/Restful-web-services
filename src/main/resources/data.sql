406e3597f52085f0507eb22c0382da73d48bcc43
gavaderavi <gavaderavi112@gmail.com>
2020-05-05 14:15:37 +0530

    My first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+insert into user values(1001,sysdate(),'AB');
+insert into user values(1002,sysdate(),'Jack');
+insert into post values(10011,'My First post',1001);
+insert into post values(10012,'My Second post',1001);
